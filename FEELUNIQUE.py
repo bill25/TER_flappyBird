12f364bc582b2fc6a6372417e6451f74f658ed9b
Salsabil AOUISSAT <salsabilaouissat@gmail.com>
2018-12-28 13:47:27 +0100

    initialisation du projet

---
+++ FEELUNIQUE.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Dec 26 19:56:29 2018
+
+@author: Aouissat_salsabil
+"""
+import pandas as pd 
+FEELUNIQUE=pd.read_csv('base_feelunique.txt',sep=",",encoding = "latin1")
