501d9e49504c69510b4b3d09a5d5870f8e834962
Salsabil AOUISSAT <salsabilaouissat@gmail.com>
2018-12-28 13:50:59 +0100

    nouveau changement

--- FEELUNIQUE.py
+++ FEELUNIQUE.py
@@ -5,4 +5,8 @@ Created on Wed Dec 26 19:56:29 2018
 @author: Aouissat_salsabil
 """
 import pandas as pd 
-FEELUNIQUE=pd.read_csv('base_feelunique.txt',sep=",",encoding = "latin1")
+feelunique=pd.read_csv('base_feelunique.txt',sep=";",encoding = "latin1")
+feelunique.describe()
+feelunique.info()
+feelunique.isnull().sum()
+#first commentaire
